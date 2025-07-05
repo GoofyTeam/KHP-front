50de16600c9b8356351c4ec28c7b38d33498a09e
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2025-07-06 00:59:04 +0200

    [KHP-41] feat(pwa): create HttpClient & make login page

--- apps/pwa/vite.config.ts
+++ apps/pwa/vite.config.ts
@@ -1,11 +1,16 @@
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react(),
     tailwindcss(),
     VitePWA({
