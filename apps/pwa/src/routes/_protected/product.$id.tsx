48d380e5830514874a0f6d19264f3191a7a99fde
lucag322 <97728486+lucag322@users.noreply.github.com>
2025-07-08 00:23:03 +0200

    [KHP-45] feat(pwa): setup product detail page

--- apps/pwa/src/routes/_protected/product.$id.tsx
+++ apps/pwa/src/routes/_protected/product.$id.tsx
@@ -1,18 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { StockStatus } from "@workspace/ui/components/stock-status";
 
 export const Route = createFileRoute("/_protected/product/$id")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { id } = Route.useParams();
-
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold text-khp-text-primary mb-4">
-        Produit {id}
-      </h2>
-      <p className="text-gray-600">Détails du produit avec l'ID : {id}</p>
+    <div className="p-6 flex flex-col gap-4 ">
+      <div className="flex flex-col justify-center items-center">
+        <img
+          src="https://images.openfoodfacts.org/images/products/761/303/666/8910/front_fr.164.400.jpg"
+          alt="Produit"
+          className="aspect-square object-contain max-w-1/2 w-full"
+        />
+      </div>
+      <div className="flex flex-col gap-1 ">
+        <h2 className="text-2xl font-semibold">Nom du produit</h2>
+        <p>Categorie : Aliments en conserve</p>
+      </div>
+      <div className="flex justify-between gap-1 items-center">
+        <p>Quantity : 17 KG</p>
+        <StockStatus variant="out-of-stock" />
+      </div>
     </div>
   );
 }
