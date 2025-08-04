c9687fcab2fd0ff591b9886ad5632dac07c61b47
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2025-08-04 18:13:04 +0200

    [KHP-75] feat(pwa): Add inventory search

--- apps/pwa/src/pages/Inventory.tsx
+++ apps/pwa/src/pages/Inventory.tsx
@@ -1,17 +1,62 @@
-import { useLoaderData } from "@tanstack/react-router";
+import {
+  Link,
+  Route,
+  useLoaderData,
+  useNavigate,
+  useSearch,
+} from "@tanstack/react-router";
 import InventoryRow from "../components/inventory-row";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
+import { ScanBarcode } from "lucide-react";
+import { useDebounce } from "@uidotdev/usehooks";
+import { useEffect, useState } from "react";
 
 function InventoryPage() {
   const { ingredients } = useLoaderData({
     from: "/_protected/inventory",
   });
+  const navigate = useNavigate({ from: "/inventory" });
+  const { search_terms = "", pageIndex } = useSearch({
+    from: "/_protected/inventory",
+  });
+
+  const [searchTerm, setSearchTerm] = useState(search_terms || "");
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
+  useEffect(() => {
+    navigate({
+      search: {
+        search_terms: debouncedSearchTerm || undefined,
+      },
+    });
+  }, [debouncedSearchTerm, navigate]);
+
+  console.log("InventoryPage loaded with ingredients:", ingredients);
 
   return (
-    <>
-      {ingredients.map((ingredient) => (
+    <div>
+      {ingredients.data.map((ingredient) => (
         <InventoryRow key={ingredient.id} productDetails={ingredient} />
       ))}
-    </>
+
+      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 max-w-md bg-background p-4 rounded shadow-lg w-11/12 flex items-center justify-start gap-2">
+        <Input
+          type="text"
+          placeholder="Search for ingredients..."
+          variant="khp-default"
+          className=""
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <Button variant="khp-default" asChild>
+          <Link to="/scan">
+            <ScanBarcode size={32} />
+          </Link>
+        </Button>
+      </div>
+    </div>
   );
 }
 
--- apps/pwa/src/routes/_protected/inventory.tsx
+++ apps/pwa/src/routes/_protected/inventory.tsx
@@ -5,17 +5,43 @@ import {
   GetCompanyProducts,
   GetCompanyProductsQuery,
 } from "../../graphql/getCompanyProducts.gql";
+import z from "zod";
 
 export const Route = createFileRoute("/_protected/inventory")({
-  loader: async () => {
-    const data =
-      await graphqlRequest<GetCompanyProductsQuery>(GetCompanyProducts);
+  validateSearch: z.object({
+    search_terms: z.string().optional(),
+    pageIndex: z.number().default(1),
+  }),
+  loaderDeps: ({ search }) => ({
+    search_terms: search.search_terms,
+    pageIndex: search.pageIndex,
+  }),
+  loader: async ({ deps: { search_terms, pageIndex } }) => {
+    let options = {};
 
-    const ingredients = data.ingredients.data ?? [];
+    if (search_terms && search_terms.trim() !== "") {
+      options = {
+        ...options,
+        name: search_terms.trim(),
+      };
+    }
 
-    return {
-      ingredients,
-    };
+    if (pageIndex < 1) {
+      options = {
+        ...options,
+        page: 1,
+      };
+    } else {
+      options = {
+        ...options,
+        page: pageIndex,
+      };
+    }
+
+    return await graphqlRequest<GetCompanyProductsQuery>(
+      GetCompanyProducts,
+      options
+    );
   },
   component: InventoryPage,
 });
