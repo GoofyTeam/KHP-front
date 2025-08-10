0910e91b52993ca5f4e19fdfbe32919647312884
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2025-08-10 05:32:00 +0200

    [KHP-000] feat(pwa): add typed GraphQL module declarations

--- apps/pwa/src/types/global_declaration.d.ts
+++ apps/pwa/src/types/global_declaration.d.ts
@@ -1,2 +1,11 @@
-declare module "*.gql";
-declare module "*.graphql";
+declare module "*.gql" {
+  import type { DocumentNode } from "graphql";
+  const value: DocumentNode;
+  export default value;
+}
+declare module "*.graphql" {
+  import type { DocumentNode } from "graphql";
+  const value: DocumentNode;
+  export default value;
+}
+
