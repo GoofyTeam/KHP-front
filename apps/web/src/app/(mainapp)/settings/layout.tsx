bef958d794e253910bbb254a3a6a874141977bb0
lucag322 <97728486+lucag322@users.noreply.github.com>
2025-08-31 20:31:45 +0200

    [KHP-135] feat(web): enhance account actions with logout functionality and improve error handling

--- apps/web/src/app/(mainapp)/settings/layout.tsx
+++ apps/web/src/app/(mainapp)/settings/layout.tsx
@@ -1,4 +1,5 @@
 import { SettingsSidebar } from "@/components/settings-sidebar";
+import { ScrollArea } from "@workspace/ui/components/scroll-area";
 
 export default function SettingsLayout({
   children,
@@ -10,7 +11,11 @@ export default function SettingsLayout({
       <div className="flex-shrink-0">
         <SettingsSidebar />
       </div>
-      <main className="flex-1 min-w-0">{children}</main>
+      <main className="flex-1 min-w-0 h-full">
+        <ScrollArea className=" max-h-[calc(100vh-100px)] h-full w-full">
+          {children}
+        </ScrollArea>
+      </main>
     </div>
   );
 }
