4b256c5eabb2f905b85f82123120393e4bf74670
Hiteazel <khacminhbrandon.vo@gmail.com>
2025-09-15 15:56:04 +0200

    [KHP-222] fix(web): using AutoBreadcrumbs in stocks/add

--- apps/web/src/app/(mainapp)/stocks/layout.tsx
+++ apps/web/src/app/(mainapp)/stocks/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import StocksBreadcrumbs from "./Breadcrumbs";
+import { AutoBreadcrumb } from "@/components/auto-breadcrumb";
 
 export const metadata: Metadata = {
   title: "KHP | Stocks",
@@ -15,7 +15,12 @@ export default function StocksLayout({
     <div className="flex flex-col h-full">
       <header className="border-b border-khp-secondary bg-background sticky top-0 z-10">
         <div className="px-6 py-4 space-y-3">
-          <StocksBreadcrumbs />
+          <AutoBreadcrumb
+            listClassName="text-xl font-semibold"
+            overrides={{
+              "/stocks/add": "Add to stock",
+            }}
+          />
         </div>
       </header>
       <div className="flex-1 overflow-auto p-4">{children}</div>
